a8503be1f7d23fdd7b20626b697bd9eb54e151f6
victor mann <victorcmann@gmail.com>
2021-01-14 16:46:19 -0300

    menu layout

--- src/components/Estruture/Menu/index.js
+++ src/components/Estruture/Menu/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { MenuArea } from './styled';
+import { MenuArea, MenuItem } from './styled';
 import { Container } from '../../mainComponents';
+import { asset, page } from '../../../helpers';
 
 function Menu() {
 	return (
 		<MenuArea>
 			<Container>
-				<div>
-					menu
+				<div className="flx flx-align-center">
+					<nav>
+						<span className="btn-nav">Navegue pela loja</span>
+					</nav>
+					<button className="btn-mar">Nossas Marcas</button>
+					<ul className="cat-list list-un flx">
+						<MenuItem 
+							name="Bicicletas" 
+							link={page('categoria/bicicletas')} 
+							image={asset('static/images/mic-bicicletas-w.png')} />
+						<MenuItem 
+							name="Proteção e Segurança" 
+							link={page('categoria/protecao-e-seguranca')} 
+							image={asset('static/images/mic-protecao-e-seguranca-w.png')} />
+						<MenuItem 
+							name="Treinamento" 
+							link={page('categoria/treinamento')} 
+							image={asset('static/images/mic-treinamentos-w.png')} />
+						<MenuItem 
+							name="Calçados" 
+							link={page('categoria/calcados')} 
+							image={asset('static/images/mic-calcados-w.png')} />
+					</ul>
 				</div>
 			</Container>
 		</MenuArea>
--- src/components/Estruture/Menu/styled.js
+++ src/components/Estruture/Menu/styled.js
@@ -1,8 +1,82 @@
 import styled from 'styled-components';
 import { color } from '../../mainComponents';
+import { Link } from 'react-router-dom';
 
+import { asset } from '../../../helpers';
 
 export const MenuArea = styled.nav`
 background-color: ${color.first};
 color: white;
+
+.btn-nav {
+  display: inline-block;
+  text-transform: uppercase;
+  font-weight: bold;
+  padding: .8em;
+  cursor: pointer;
+
+  &::before,
+  &::after {
+    content: '';
+    display: inline-block;
+    width: 1.5em;
+    height: 1.5em;
+    vertical-align: middle;
+    margin: 0 .3rem;
+  }
+
+  &::before {
+    background: url(${asset('static/images/ciclista-white.png')}) no-repeat center;
+    background-size: contain;
+  }
+
+  &::after {
+    background: url(${asset('static/images/arrow-point-to-right-white.png')}) no-repeat center;
+    background-size: 50%;
+  }
+}
+.btn-mar {
+  display: inline-block;
+  background-color: transparent;
+  border: 1px solid #ddd;
+  color: #fff;
+  padding: .5rem;
+  border-radius: 3px;
+  margin-left: 3em;
+  margin-right: 3em;
+  cursor: pointer;
+}
 `;
+
+const AreaMenuItem = styled.li`
+a {
+    display: inline-block;
+    color: #fff;
+    text-decoration: none;
+    padding: .6rem;
+
+    &:hover {
+      background-color: #323e4a;
+    }
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 2em;
+      height: 2em;
+      background-position: center;
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-image: url(${props => props.image});
+      vertical-align: middle;
+      margin-right: .2em;
+    }
+  }
+`;
+export const MenuItem = ({name, link, image}) => {
+  return (
+    <AreaMenuItem image={image}>
+      <Link to={link}>{name}</Link>
+    </AreaMenuItem>
+  )
+};
